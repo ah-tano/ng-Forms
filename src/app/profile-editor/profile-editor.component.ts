159f645812fea4ac1802d26118577ac8f667b0ad
Nastiia <lanastasialysenkol@gmail.com>
2020-06-03 22:22:14 +0300

    inject FormBuilder service

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -7,17 +7,29 @@ import {FormGroup, FormControl, FormBuilder} from '@angular/forms';
   styleUrls: ['./profile-editor.component.css']
 })
 export class ProfileEditorComponent implements OnInit {
-  profileForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl('')
-    })
-  })
-  constructor() { }
+  // profileForm = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   address: new FormGroup({
+  //     street: new FormControl(''),
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     zip: new FormControl('')
+  //   })
+  // })
+
+  profileForm = this.fb.group({
+   firstName: [''],
+   lastName: [''],
+   address: this.fb.group({
+     street: [''],
+     city: [''],
+     state: [''],
+     zip: ['']
+   }),
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
